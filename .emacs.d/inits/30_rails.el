4cb3a69b25384c991267661626f31551783d545d
nagoshi <tamakoron7@gmail.com>
2014-09-29 17:32:29 +0900

    rails add ?

---
+++ .emacs.d/inits/30_rails.el
@@ -0,0 +1,20 @@
+(require 'rbenv)
+(require 'projectile)
+(require 'projectile-rails)
+;; 
+;;
+
+;; (setq rbenv-show-active-ruby-in-modeline nil)
+(setq rbenv-modeline-function 'rbenv--modeline-plain)
+
+
+(add-hook 'projectile-mode-hook 'projectile-rails-on)
+;; ;; rails project 始めるときに呼び出す
+
+(defun my/rails-start ()
+  (global-rbenv-mode)
+  (projectile-global-mode)
+
+  )
+
+
