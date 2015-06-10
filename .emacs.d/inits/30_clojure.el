e98bc918af79e0d7ff390f10d0e1fa1632362e77
ztama <tamakoron7@gmail.com>
2015-06-11 03:33:30 +0900

    emacs sql client && my-password.gpg

--- .emacs.d/inits/30_clojure.el
+++ .emacs.d/inits/30_clojure.el
@@ -1,7 +1,10 @@
-(add-hook 'clojure-mode-hook 'cider-mode)
+(require 'clojure-mode)
+(require 'cider)
 
-;; mini bufferに関数の引数を表示させる
-(add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
+(require 'clojure-cheatsheet)
+(require 'slamhound)
+
+
 
 ;; 'C-x b' した時に *nrepl-connection* と *nrepl-server* のbufferを一覧に表示しない
 (setq nrepl-hide-special-buffers t)
@@ -9,8 +12,23 @@
 ;; RELPのbuffer名を 'project名:nREPLのport番号' と表示する
 ;; project名は project.clj で defproject した名前
 (setq nrepl-buffer-name-show-port t)
+(add-hook 'clojure-mode-hook 'cider-mode)
+(add-hook 'cider-mode-hook #'eldoc-mode)
+(setq cider-show-error-buffer 'except-in-repl)
+(setq ac-quick-help-delay 0.5)
+(add-hook 'cider-mode-hook 'ac-flyspell-workaround)
+;; 
+;; ;; mini bufferに関数の引数を表示させる
+;; (add-hook 'cider-mode-hook #'cider-turn-on-eldoc-mode)
 
-(autoload 'ac-cider "ac-cider" nil t)
+
+
+;;;; ----------------------------
+;;;; AAAAAAAAACCCCCCCCCCCC
+(require 'auto-complete-config)
+(ac-config-default)
+
+(require 'ac-cider)
 (add-hook 'cider-mode-hook 'ac-flyspell-workaround)
 (add-hook 'cider-mode-hook 'ac-cider-setup)
 (add-hook 'cider-repl-mode-hook 'ac-cider-setup)
@@ -18,6 +36,26 @@
   '(progn
      (add-to-list 'ac-modes 'cider-mode)
      (add-to-list 'ac-modes 'cider-repl-mode)))
+
+(defun set-auto-complete-as-completion-at-point-function ()
+  (setq completion-at-point-functions '(auto-complete)))
+
+(add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
+(add-hook 'cider-mode-hook 'set-auto-complete-as-completion-at-point-function)
+;;;; ----------------------------
+
+(defun clojure-auto-complete ()
+  (interactive)
+  (let ((ac-sources
+         `(ac-source-nrepl-ns
+           ac-source-nrepl-vars
+           ac-source-nrepl-ns-classes
+           ac-source-nrepl-all-classes
+           ac-source-nrepl-java-methods
+           ac-source-nrepl-static-methods
+           ,@ac-sources)))
+    (auto-complete)))
+
 
 ;; M-x clojure-cheatsheetで以下のサイトにある情報(同じもの？)を参照できる。 helm 可
 ;; M-x slamhoundすると , 必要なpackageを自動でrequrieしてくれるツール。
