2576ea9389fed2754a765b57a1b799d9d59a7332
nagoshi <tamakoron7@gmail.com>
2015-06-17 04:03:04 +0900

    emacs font fix

--- .emacs.d/inits/80_font.el
+++ .emacs.d/inits/80_font.el
@@ -1,5 +1,17 @@
 
 ;;;;フォントの設定
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;Source Han Code JP
+;;123 123 123 123 123123123123
+;;あい うえ おか きく
+;;''' ||| @@@ jfe
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; | aaaいう  | bngs     |
+;; | fjdsa   | jdfあfda |
+
+
 ;;::::::::::::::
 ;;^^^^^^^^^^^^^^
 ;;;;;;;;;;;;;;;;
@@ -29,10 +41,13 @@
 ;;  フォントファミリ (pp (font-family-list))
 ;; (setq default-frame-alist '((font . "VL ゴシック-16")))
 (cond
- ((or 
-   (equal system-name "utrtamako" )
-   (equal system-name "zenlap.zendomain" )
+ ( (equal system-name "utrtamako" )
+   ;; (setq default-frame-alist '((font . "Source Han Code JP")))
+   ;; (set-frame-font "Source Han Code JP-16")
+   (setq default-frame-alist '((font . "Ricty Discord4Powerline:pixelsize=16")))
+   (set-frame-font "Ricty Discord4Powerline:pixelsize=16")
    )
+ ((equal system-name "zenlap.zendomain" )
   (setq default-frame-alist '((font . "Migu 1m")))
   (set-frame-font "Migu 1m-18")
   ;; (set-face-attribute 'default nil
@@ -40,40 +55,37 @@
   ;;   	    :height 120)
   
   (set-face-attribute 'default nil
-                    :family "Migu 1m"
-                    :height 150)
-;;  (set-fontset-font (frame-parameter nil 'font)
-;;                  'japanese-jisx0208
-;;                  (cons "Migu 1m" "iso10646-1"))
-;;  (set-fontset-font (frame-parameter nil 'font)
-;;                  'japanese-jisx0212
-;;                  (cons "Migu 1m" "iso10646-1"))
-;;  (set-fontset-font (frame-parameter nil 'font)
-;;                  'katakana-jisx0201
-;;                  (cons "Migu 1m" "iso10646-1"))
+                      :family "Migu 1m"
+                      :height 150)
+  ;;  (set-fontset-font (frame-parameter nil 'font)
+  ;;                  'japanese-jisx0208
+  ;;                  (cons "Migu 1m" "iso10646-1"))
+  ;;  (set-fontset-font (frame-parameter nil 'font)
+  ;;                  'japanese-jisx0212
+  ;;                  (cons "Migu 1m" "iso10646-1"))
+  ;;  (set-fontset-font (frame-parameter nil 'font)
+  ;;                  'katakana-jisx0201
+  ;;                  (cons "Migu 1m" "iso10646-1"))
   )
 
-
-  ((equal system-name "localhostss")
-   (set-frame-font "Ricty-12")
-   (set-face-attribute 'default nil
-     	    :family "Ricty"
-     	    :height 120)
-  (setq default-frame-alist '((font . "Ricty-12")))
-  )
-  ( (or (equal system-name "localhost.localdomain")
-        (equal system-name "localhost.homenetwork"))
-   (set-frame-font "Ricty Discord4Powerline-12")
-   (set-face-attribute 'default nil
-     	    :family "Ricty Discord4Powerline"
-     	    :height 120)
-  (setq default-frame-alist '((font . "Ricty Discord4Powerline-12")))
-  )
-  
-  )
-
-
+ ((equal system-name "localhostss")
+  (set-frame-font "Ricty-12")
+  (set-face-attribute 'default nil
+                      :family "Ricty"
+                      :height 120)
+  (setq default-frame-alist '((font . "Ricty-12"))))
  
+ ((some (lambda (strs) (equal system-name strs))
+        '("localhost.localdomain"
+          "localhost.homenetwork"))
+  
+  (set-frame-font "Ricty Discord4Powerline-12")
+  (set-face-attribute 'default nil
+                      :family "Ricty Discord4Powerline"
+                      :height 120)
+  (setq default-frame-alist '((font . "Ricty Discord4Powerline-12")))))
+
+
 
 
 
