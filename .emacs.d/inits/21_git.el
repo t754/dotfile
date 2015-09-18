07f4cc2ebc75abd07cc0f697a96de1cb30edd6e4
tamakoron <tamakoron7@gmail.com>
2015-09-18 10:25:08 +0900

    magit mode fix

--- .emacs.d/inits/21_git.el
+++ .emacs.d/inits/21_git.el
@@ -1,17 +1,19 @@
-;; (require 'magit)
+(require 'magit)
 
 ;; (require 'git-gutter)
 ;;;; (require 'git-gutter-fringe)
 ;;;; (setq git-gutter-fr:side 'right-fringe)
 ;;
-(with-eval-after-load "git-gutter"
-  (bind-keys :map global-map
+(bind-keys :map global-map
              ("C-x C-g" . git-gutter)
              ("C-x v =" . git-gutter:popup-hunk)
              ("C-x p" . git-gutter:previous-hunk)
              ("C-x n" . git-gutter:next-hunk)
              ("C-x v s" . git-gutter:stage-hunk)
-             ("C-x v r" . git-gutter:revert-hunk) )
+             ("C-x v r" . git-gutter:revert-hunk)
+             ("C-x g" . magit-status))
+;; (with-eval-after-load "git-gutter"
+;;   )
 
   (smartrep-define-key goto-map "M-f"
     '(("n"      . 'git-gutter:next-hunk)
@@ -20,7 +22,8 @@
   ;; '(git hg )
 
   (setq git-gutter:handled-backends '(git hg)))
-
+(with-eval-after-load 'magit
+  (global-git-commit-mode +1))
 (global-git-gutter-mode t)
 ;; ;;;;;;;;;;;;;;;;;;;;;;;;
 ;; ;; 何故か ediffの設定   ;;
