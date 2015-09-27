8f78975d225513fba20690c2b574276ef4deca71
tamakoron <tamakoron7@gmail.com>
2015-09-28 07:26:48 +0900

    smex fix

--- .emacs.d/inits/20_smex.el
+++ .emacs.d/inits/20_smex.el
@@ -9,10 +9,16 @@
 ;;;;; M-. jumps to the definition of the selected command.
 ;;;
 ;;;;; C-h w shows the key bindings for the selected command. (Via where-is.)
-
+(setq ido-max-window-height 0.75
+      ido-enable-flex-matching t
+      ido-vertical-define-keys 'C-n-and-C-p-only)
+(ido-vertical-mode 1)
 (smex-initialize)
-(global-set-key (kbd "M-x") 'smex)
-(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+(bind-key "M-x" 'smex)
+(bind-key "M-X" 'smex-major-mode-commands)
+
+;; (global-set-key (kbd "M-x") 'smex)
+;; (global-set-key (kbd "M-X") 'smex-major-mode-commands)
 (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
 
 ;; (defadvice smex (around space-inserts-hyphen activate compile)
