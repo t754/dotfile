2da1d3c304eaed1bd9a1dc0c873cbe6c6003c090
nagoshi <tamakoron7@gmail.com>
2015-10-02 17:37:47 +0900

    helm-c-yasnnippet → ido

--- .emacs.d/inits/20_yasnippet.el
+++ .emacs.d/inits/20_yasnippet.el
@@ -37,10 +37,31 @@
 
 ;;(define-key yas-minor-mode-map (kbd "TAB") nil)
 ;;(define-key yas-minor-mode-map (kbd "C-;") 'yas-expand)
+(defun yas-ido-expand ()
+  "Lets you select (and expand) a yasnippet key"
+  (interactive)
+    (let ((original-point (point)))
+      (while (and
+              (not (= (point) (point-min) ))
+              (not
+               (string-match "[[:space:]\n]" (char-to-string (char-before)))))
+        (backward-word 1))
+    (let* ((init-word (point))
+           (word (buffer-substring init-word original-point))
+           (list (yas-active-keys)))
+      (goto-char original-point)
+      (let ((key (remove-if-not
+                  (lambda (s) (string-match (concat "^" word) s)) list)))
+        (if (= (length key) 1)
+            (setq key (pop key))
+          (setq key (ido-completing-read "key: " list nil nil word)))
+        (delete-char (- init-word original-point))
+        (insert key)
+        (yas-expand)))))
 
 (bind-keys :map yas-minor-mode-map
-           ("C-c y" .   helm-yas-complete)
-           ("C-c i i" . helm-yas-complete);; 既存スニペットを挿入する
+           ("C-c y" .   yas-ido-expand)
+           ("C-c i i" . yas-ido-expand);; 既存スニペットを挿入する
            ;; ("C-c i i" . yas-insert-snippet);; 既存スニペットを挿入する
            ("C-c i n" . yas-new-snippet);; 新規スニペットを作成するバッファを用意する
            ("C-c i v" . yas-visit-snippet-file));; 既存スニペットを閲覧・編集する
