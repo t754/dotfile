7406004c581b20815009dc500b6190c849161f2f
nagoshi <tamakoron7@gmail.com>
2015-11-24 00:24:14 +0900

    setup.sh fix and link filename 2

--- setup.sh
+++ setup.sh
@@ -10,11 +10,6 @@ _myCheck ghq
 
 curl -fsSL https://raw.githubusercontent.com/cask/cask/master/go | python
 
-# black="/tmp/black"
-# now="/tmp/now"
-# cat blackList | sort > $black
-# ls -1 -a  > $now
-# b=`diff $black $no|w | grep ">" | sed "s/[> ]//g" `
 
 b=$(comm -13 <(sort blackList) <(ls -a -1 | sort))
 c="$PWD"
@@ -34,9 +29,6 @@ cat ghqlist | xargs -L 1 ghq get
 mkdir -p ~/.tmux/plugins
 ln -s ~/.ghq/github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-mkdir -p ~/.config/systemd/user/
-ln -s ${PWD}/emacs.service  ~/.config/systemd/user/emacs/
-
 mkdir -p ${HOME}/.urxvt/ext
 ln -s ${HOME}/.ghq/github.com/majutsushi/urxvt-font-size/font-size ${HOME}/.urxvt/ext/
 
@@ -44,8 +36,24 @@ mkdir -p ${HOME}/bin
 curl https://linux.dropbox.com/packages/dropbox.py -o "${HOME}/bin/dropbox"
 chmod +x "${HOME}/bin/dropbox"
 
-mkdir -p ${HOME}/.config/fontconfig/
-ln -s ${PWD}/conf.d ${HOME}/.config/fontconfig
 
-mkdir -p ${HOME}/.config/awesome
-ln -s ${PWD}/rc.lua ${HOME}/.config/awesome
+
+
+function _mkdir_ln(){
+    if [ $#  -ne 2 ] ; then
+        echo "usage :: _mkdir_ln src dst"
+        return 1
+    fi
+
+    mkdir -p $(dirname $2)
+    if [ -L $2 ] ; then
+        rm -v $2
+    fi
+    ln -vs $1 $2
+    return 0
+}
+export -f _mkdir_ln
+
+paste -d " " <(find $(pwd) -name "@*"| awk '{print "'\''" $0 "'\''"}') \
+             <( find . -name "@*" |  sed -e s@"./"@@ -e s/"@"// -e s."#"."/".g  -e 's/~/$/g' ) \
+  | xargs -L 1 -I@ bash -c "_mkdir_ln @"
