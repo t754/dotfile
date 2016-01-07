eb8db31174ad1a5f53321356b1036fd6c2de9641
tamakoron <tamakoron7@gmail.com>
2016-01-07 09:04:15 +0900

    org latex export fix (pdf unicode title garbling)

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -55,13 +55,36 @@
      ("" "rotating" nil)
      ("normalem" "ulem" t)
      ("" "amsmath" t)
+     "\\let\\equation\\gather
+\\let\\endequation\\endgather"
      ("" "textcomp" t)
      ("" "marvosym" t)
      ("" "wasysym" t)
      ("" "amssymb" t)
-     ("" "hyperref" nil)
-     "\\tolerance=1000"
+     "\\ifx\\kanjiskip\\undefined\\else
+  \\usepackage{atbegshi}
+  \\ifx\\ucs\\undefined
+    \\ifnum 42146=\\euc\"A4A2
+      \\AtBeginShipoutFirst{\\special{pdf:tounicode EUC-UCS2}}
+    \\else
+      \\AtBeginShipoutFirst{\\special{pdf:tounicode 90ms-RKSJ-UCS2}}
+    \\fi
+  \\else
+    \\AtBeginShipoutFirst{\\special{pdf:tounicode UTF8-UCS2}}
+  \\fi
+  \\usepackage[dvipdfmx]{hyperref}
+\\fi" "\\tolerance=1000"
      ("" "svg" nil))))
+ '(org-latex-hyperref-template
+   "\\hypersetup{
+ pdfauthor={%a},
+ hidelinks,
+ pdftitle={%t},
+ pdfkeywords={%k},
+ pdfsubject={%d},
+ pdfcreator={%c},
+ pdflang={%L}}
+")
  '(org-trello-current-prefix-keybinding "C-c o")
  '(package-selected-packages
    (quote
