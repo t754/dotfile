21d9324927658a66a88252caafd66b3db39e2ede
tamakoron <tamakoron7@gmail.com>
2016-02-14 12:24:41 +0900

    company style change

--- .emacs.d/inits/20_company.el
+++ .emacs.d/inits/20_company.el
@@ -1,11 +1,31 @@
-(when (locate-library "company")
-  (global-company-mode 1)
-  (global-set-key (kbd "C-M-i") 'company-complete)
+(with-eval-after-load 'company
+  (bind-keys :map company-active-map
+             ("C-n" . company-select-next)
+             ("C-p" . company-select-previous)
+             ("C-i" . company-complete-selection))
+  (bind-keys :map company-search-map
+             ("C-n" . company-select-next)
+             ("C-p" . company-select-previous))
+  (global-set-key (kbd "C-M-i") 'company-complete))
+(setq pos-tip-background-color "olive drab")
+(global-auto-complete-mode -1)
+(global-company-mode +1)
+(company-quickhelp-mode +1)
+
+(set-face-attribute 'company-tooltip nil
+                    :foreground "black" :background "lightgrey")
+(set-face-attribute 'company-tooltip-common nil
+                    :foreground "black" :background "lightgrey")
+(set-face-attribute 'company-tooltip-common-selection nil
+                    :foreground "white" :background "steelblue")
+(set-face-attribute 'company-tooltip-selection nil
+                    :foreground "black" :background "steelblue")
+(set-face-attribute 'company-preview-common nil
+                    :background nil :foreground "lightgrey" :underline t)
+(set-face-attribute 'company-scrollbar-fg nil
+                    :background "orange")
+(set-face-attribute 'company-scrollbar-bg nil
+                    :background "gray40")
+
   ;; (setq company-idle-delay nil) ; 自動補完をしない
-  (define-key company-active-map (kbd "C-n") 'company-select-next)
-  (define-key company-active-map (kbd "C-p") 'company-select-previous)
-  (define-key company-search-map (kbd "C-n") 'company-select-next)
-  (define-key company-search-map (kbd "C-p") 'company-select-previous)
-  (define-key company-active-map (kbd "<tab>") 'company-complete-selection))
-;;(company-quickhelp-mode +1)
-(auto-complete-mode -1)
+  ;;(company-quickhelp-mode +1)
