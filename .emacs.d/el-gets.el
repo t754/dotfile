d8df8df901647809ed3577fdee53b43cecaa7a97
tamakoron <tamakoron7@gmail.com>
2016-05-21 01:58:57 +0900

    delete file

--- .emacs.d/el-gets.el
+++ .emacs.d/el-gets.el
@@ -1,14 +1,4 @@
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-;; (require 'package)
-;; (setq package-archives '(("melpa" .  "http://melpa.org/packages/")
-;; 			   ("gnu" . "http://elpa.gnu.org/packages/")
-;; 			   ("marmalade" . "http://marmalade-repo.org/packages/")
-;; 			   ("ELPA" . "http://tromey.com/elpa/" )
-;; 			   ("org" . "http://orgmode.org/elpa/")
-;; 			   ))
-;; (package-refresh-contents)
-;; (package-initialize)
-(setq el-get-user-package-directory "~/.emacs.d/el-files/")
 (unless (require 'el-get nil 'noerror)
   (with-current-buffer
       (url-retrieve-synchronously
@@ -17,7 +7,6 @@
     (eval-print-last-sexp)))
 (require 'el-get-elpa)
 (add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
-
 
 (defvar my/el-list '()
   "A list of packages to install from el-get at launch.")
@@ -118,47 +107,12 @@
         ;;##)
         ))
 
-
-
-
-;; (cider :type github :pkgname "clojure-emacs/cider")		;;clojure-cheatsheet
-;; (el-get-bundle xxx)
-
-;;      mozc
-;;		clojure-mode-extra-font-locking
-;;		ctags-update
-;;		eldoc-extension
-;;		flycheck-clojure
-;;		flymake-haskell-multi
-;;		flymake-python-pyflakes
-;;		fold-dwim-org
-;;		git
-;;		gnuplot
-;;		helm-bm
-;;		helm-emmet
-;;		helm-projectile
-;;		http-post-simple
-;;		inf-clojure
-;;		org-plus-contrib
-;;		org-trello
-;;		ox-textile
-;;		robe
-;;		shut-up
-;;		slamhound
-;;		web-beautify
-;; melpa:ac-emoji
-;; (flymake-chicken :url "http://code.call-cc.org/cgi-bin/gitweb.cgi?p=chicken-core.git;a=blob_plain;f=misc/flymake-chicken.el")
-;; (chicken :url "http://code.call-cc.org/cgi-bin/gitweb.cgi?p=chicken-core.git;a=blob_plain;f=misc/chicken.el")
-;; cj2tszk/swank-gauche
-;; dleslie/chicken-scheme.el
 (defvar my/bundle-list '() "A list to install for el-get-bundle ")
 (setq my/bundle-list     '(
                            ;;##(
                            (emacs-codic :type github :pkgname "syohex/emacs-codic" )
                            (search-web :type github :pkgname "tomoya/search-web.el")
                            (yuutayamada/mykie-el :load-path "lisp")
-                           ;;(alexott/cedet :branch "devel" :build ("'make all'"))
-                           ;;(m0smith/malabar-mode :load-path)
                            Malabarba/beacon
                            ShingoFukuyama/emacs-emoji-cheat-sheet
                            bmag/imenu-list
@@ -178,7 +132,6 @@
                            ;;##)
                            ))
 
-(el-get-emacswiki-refresh)
 (dolist (x my/bundle-list)
   (cond
    ((listp x) (eval `(el-get-bundle ,@x)))
