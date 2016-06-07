d9056e2c1ff46e356424db663eb4e6719c6e6d6d
tamakoron <tamakoron7@gmail.com>
2016-06-08 03:27:16 +0900

    add () rainbow color

--- .emacs.d/inits/30_lisp.el
+++ .emacs.d/inits/30_lisp.el
@@ -11,3 +11,14 @@
   (define-key company-active-map (kbd "M-.") 'company-show-location)
   (define-key slime-prefix-map (kbd "M-h") 'slime-documentation-lookup)
   (setq inferior-lisp-program "ros -L sbcl -Q run"))
+
+(require 'cl-lib)
+(require 'color)
+(defun rainbow-delimiters-using-stronger-colors ()
+  (interactive)
+  (cl-loop
+   for index from 1 to rainbow-delimiters-max-face-count
+   do
+   (let ((face (intern (format "rainbow-delimiters-depth-%d-face" index))))
+    (cl-callf color-saturate-name (face-foreground face) 80))))
+(add-hook 'emacs-startup-hook 'rainbow-delimiters-using-stronger-colors)
