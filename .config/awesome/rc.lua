5cab0946ca1d9d22f01d40e91d80333ed9246262
tamakoron <tamakoron7@gmail.com>
2016-08-13 02:19:22 +0900

    fix awesome wm  class rule

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -577,7 +577,7 @@ root.keys(globalkeys)
 awful.rules.rules = {
     -- All clients will match this rule.
     { rule = { },
-      properties = { border_width = beautiful.border_width * 4,
+      properties = { border_width = beautiful.border_width,
                      border_color = beautiful.border_normal,
                      focus = awful.client.focus.filter,
                      raise = true,
@@ -594,9 +594,10 @@ awful.rules.rules = {
       properties = { floating = true } },
     { rule = { class = "gimp" },
       properties = { floating = true } },
-    -- Set Firefox to always map on tags number 2 of screen 1.
-    -- { rule = { class = "Firefox" },
-    --   properties = { tag = tags[1][2] } },
+    { rule = { class = "Keepassx" },
+      properties = { tag = tags[1][9] } },
+    { rule = { class = "Firefox" },
+      properties = { tag = tags[1][2] } },
 }
 -- }}}
 
