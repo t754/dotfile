6992f3db0018722468c7363ad6383572c98be226
tamakoron <tamakoron7@gmail.com>
2017-07-16 02:57:13 +0900

    change layout

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -123,22 +123,16 @@ menubar.geometry = {
    height = 32
 }
 -- Table of layouts to cover with awful.layout.inc, order matters.
-awful.layout.layouts =
-   {
 
-      -- awful.layout.suit.tile,
-      awful.layout.suit.tile.left,
-      awful.layout.suit.tile.bottom,
-      -- awful.layout.suit.tile.top,
-      -- awful.layout.suit.fair,
-      -- awful.layout.suit.fair.horizontal,
-      -- awful.layout.suit.spiral,
-      -- awful.layout.suit.spiral.dwindle,
-      -- awful.layout.suit.max,
-      -- awful.layout.suit.floating,
-      awful.layout.suit.max.fullscreen,
-      -- awful.layout.suit.magnifier
-   }
+
+
+awful.layout.layouts = {
+    awful.layout.suit.tile.right,
+    awful.layout.suit.corner.nw,
+    awful.layout.suit.tile.bottom,
+    awful.layout.suit.max,
+}
+
 local function client_menu_toggle_fn()
    local instance = nil
    return function ()
