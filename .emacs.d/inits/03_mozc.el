513cd695ad55aeee9cd3fa473fbd47dd4758123f
tamakoron <tamakoron7@gmail.com>
2017-11-05 00:27:33 +0900

    support mozc & anthy input-mode

--- .emacs.d/inits/03_mozc.el
+++ .emacs.d/inits/03_mozc.el
@@ -1,45 +1,47 @@
-(add-to-list 'load-path "/usr/share/emacs/site-lisp/emacs-mozc")
+(add-to-list 'load-path "/usr/share/emacs/site-lisp/mozc")
+(add-to-list 'load-path "/usr/share/emacs/site-lisp/anthy")
 
-(when  (require 'mozc nil t)
-  (set-buffer-file-coding-system 'utf-8)
-  (set-buffer-file-coding-system 'utf-8)
-  (set-clipboard-coding-system 'utf-8)
-  (set-default-coding-systems 'utf-8)
-  (set-keyboard-coding-system 'utf-8)
-  (set-language-environment "Japanese")
-  (set-terminal-coding-system 'utf-8)
-  (prefer-coding-system 'utf-8-unix)
+(setq input-cursor-color "PowderBlue")
+(cond ((require 'mozc nil t)
+       (setq mozc-color input-cursor-color
+             default-input-method "japanese-mozc"
+             mozc-candidate-style 'echo-area)
+       (global-set-key (kbd "C-\\") 'toggle-input-method)
+       (bind-keys :map mozc-mode-map
+                  ("C-g" . toggle-input-method)))
 
-  (setq default-buffer-file-coding-system 'utf-8
-        mozc-color "PowderBlue"
-        default-input-method "japanese-mozc"
-        mozc-candidate-style 'echo-area)
+      ((require 'anthy nil t)
+       (defvaralias 'last-command-char 'last-command-event)
+       (set-language-environment "Japanese")
+       (setq default-input-method "japanese-anthy")
+       (global-set-key (kbd "C-\\") 'toggle-input-method)
+       (anthy-load-hiragana-map anthy-alt-char-map)
+       (set-language-environment "Japanese")
+       ;;変換時の文字の色
+       (set-face-foreground 'anthy-highlight-face "white")
+       (set-face-background 'anthy-highlight-face "black")
+       ;;アンダーライン消去
+       (set-face-underline 'anthy-highlight-face nil)
+       (set-face-underline 'anthy-underline-face nil)
+       (set-face-background 'anthy-underline-face "dark blue")))
 
-  (global-set-key (kbd "C-\\") 'toggle-input-method)
-  (bind-keys :map mozc-mode-map
-             ("C-g" . toggle-input-method)))
+(set-buffer-file-coding-system 'utf-8)
+(set-clipboard-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(prefer-coding-system 'utf-8-unix)
+(setq default-buffer-file-coding-system 'utf-8)
 
+(defmacro unbound?(x)
+  `(and (boundp ',x) ,x))
 
+(defun change-cursor-color ()
+  (cond ((unbound? mozc-mode) (set-cursor-color mozc-color))
+        ((unbound? anthy-minor-mode) (set-cursor-color input-cursor-color))
+        (t (set-cursor-color "orange"))))
 
-;; helm で候補のアクションを表示する際に IME を OFF にする
-(defadvice helm-select-action (before ad-helm-select-action-for-mozc activate)
-  (if (>= (string-to-number emacs-version) 24.3)
-      (deactivate-input-method)
-    (inactivate-input-method)))
-
-
-
-(defun mozc-change-cursor-color ()
-    (if mozc-mode
-        (set-cursor-color mozc-color)
-      (set-cursor-color "orange")
-      ))
-
-(defun input-method-activate-hooks ()
-  ;; 何もしなかったら 3sec で disable
-  (run-with-idle-timer 3 nil #'deactivate-input-method)
-  (mozc-change-cursor-color))
-
-(add-hook 'input-method-activate-hook 'input-method-activate-hooks)
-
-(add-hook 'input-method-inactivate-hook 'mozc-change-cursor-color)
+(add-hook 'input-method-activate-hook
+          (lambda () (change-cursor-color)))
+(add-hook 'input-method-inactivate-hook
+          (lambda() (change-cursor-color)))
