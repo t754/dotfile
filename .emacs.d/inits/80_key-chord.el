96c4d76d15669cfe88c6e6dad534f26fe62e8fe1
t754 <tamakoron7@gmail.com>
2018-03-05 22:11:03 +0900

    adjust keychord time

--- .emacs.d/inits/80_key-chord.el
+++ .emacs.d/inits/80_key-chord.el
@@ -1,6 +1,6 @@
 (require 'key-chord)
 (key-chord-mode 1)
-(setq key-chord-two-keys-delay 0.05)
+(setq key-chord-two-keys-delay 0.07)
 (key-chord-define-global "jk" 'avy-goto-char)
 ;; (key-chord-define-global "uu" 'undo-tree-visualize)
 ;; ("M-i"     . )
