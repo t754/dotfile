b20e08b10272c7d96ae000425c503e4ae74b8758
t_nagoshi <t_nagoshi@timedia.co.jp>
2019-04-22 17:05:22 +0900

    fix rails-mode

--- .emacs.d/inits/30_rails.el
+++ .emacs.d/inits/30_rails.el
@@ -1,17 +1,19 @@
-;; (require 'rbenv)
+(require 'rbenv)
+;; (require 'bundle)
 ;; (require 'projectile)
-;; (require 'projectile-rails)
-;; (require 'helm-rails)
-;; ;;
-;; ;;
+(require 'projectile-rails)
+
+
 
 ;; ;; (setq rbenv-show-active-ruby-in-modeline nil)
-;; (setq rbenv-modeline-function 'rbenv--modeline-plain)
+(setq rbenv-modeline-function 'rbenv--modeline-plain)
+
 
 
 ;; (add-hook 'projectile-mode-hook 'projectile-rails-on)
 ;; ;; ;; rails project 始めるときに呼び出す
 
-;; (defun my/rails-start ()
-;;   (global-rbenv-mode)
-;;   (projectile-global-mode))
+(defun my/rails-start ()
+  (interactive)
+  (global-rbenv-mode)
+  (projectile-rails-mode t))
