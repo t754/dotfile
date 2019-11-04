733dc3d72f2af0fc2c68f022a75a0bfccc70ddb5
t_754 <tamakoron7@gmail.com>
2019-11-05 02:45:17 +0900

    delete define

--- fortitude60.c
+++ fortitude60.c
@@ -1,29 +1,5 @@
 #include QMK_KEYBOARD_H
 
-#define MOUSEKEY_DELAY             0
-#define MOUSEKEY_INTERVAL          1
-#define MOUSEKEY_MAX_SPEED         10000
-#define MOUSEKEY_TIME_TO_MAX       2
-#define MOUSEKEY_WHEEL_MAX_SPEED   80
-#define MOUSEKEY_WHEEL_TIME_TO_MAX 400
-
-#define MK_3_SPEED
-#define MK_MOMENTARY_ACCEL // comment this out for tap-to-select acceleration
-// cursor speeds:
-#define MK_C_OFFSET_SLOW     1 // pixels
-#define MK_C_INTERVAL_SLOW 100 // milliseconds
-#define MK_C_OFFSET_MED      4
-#define MK_C_INTERVAL_MED   16
-#define MK_C_OFFSET_FAST    12
-#define MK_C_INTERVAL_FAST  16
-// scroll wheel speeds:
-#define MK_W_OFFSET_SLOW     1 // wheel clicks
-#define MK_W_INTERVAL_SLOW 400 // milliseconds
-#define MK_W_OFFSET_MED      1
-#define MK_W_INTERVAL_MED  200
-#define MK_W_OFFSET_FAST     1
-#define MK_W_INTERVAL_FAST 100
-
 extern keymap_config_t keymap_config;
 
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
