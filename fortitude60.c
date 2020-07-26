a27a8fbcd2bb1dbb9635193124c079ccbd110094
t_754 <tamakoron7@gmail.com>
2020-07-27 03:44:19 +0900

    fix fortitude

--- fortitude60.c
+++ fortitude60.c
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                |------+------+------+------+------+------|
  * | LCTL |LT(_RA|  S   |  D   |  F   |  G   |                |  H   |  J   |  K   |  L   |LT(_ME| QUOT |
  * |------+------+------+------+------+------+------.  ,------+------+------+------+------+------+------|
- * | LSFT |  Z   |  X   |  C   |  V   |  B   | GRV  |  | ESC  |  N   |  M   | COMM | DOT  | SLSH | ENT  |
+ * | LSFT |  Z   |  X   |  C   |  V   |  B   | GRV  |  | ESC  |  N   |  M   | COMM | DOT  | SLSH | RSFT |
  * `------+------+------+------+------+------+------.  ,------+------+------+------+------+------+------'
  *               |LOWER | LALT |GUI_T(| SPC  | BSPC |  | TAB  | ENT  |M_HENK| DOWN |  LT  |
  *               `----------------------------------'  `----------------------------------'
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_EQL,  KC_1,  KC_2,    KC_3,    KC_4,            KC_5,                                 KC_6,                KC_7,  KC_8,    KC_9,   KC_0,                KC_BSLS, \
   KC_TAB,  KC_Q,  KC_W,    KC_E,    KC_R,            KC_T,                                 KC_Y,                KC_U,  KC_I,    KC_O,   KC_P,                KC_MINS, \
   KC_LCTL, KC_A,  KC_S,    KC_D,    KC_F,            KC_G,                                 KC_H,                KC_J,  KC_K,    KC_L,   LT(_MEDIA, KC_SCLN), KC_QUOT, \
-  KC_LSFT, KC_Z,  KC_X,    KC_C,    KC_V,            KC_B,   KC_GRV,  LT(_MEDIA, KC_ESC),  KC_N,                KC_M,  KC_COMM, KC_DOT, KC_SLSH,             KC_ENT, \
+  KC_LSFT, KC_Z,  KC_X,    KC_C,    KC_V,            KC_B,   KC_GRV,  LT(_MEDIA, KC_ESC),  KC_N,                KC_M,  KC_COMM, KC_DOT, KC_SLSH,             KC_RSFT, \
                   LOWER,   KC_LALT, LGUI_T(KC_MHEN), KC_SPC, KC_BSPC, KC_TAB, LT(_RAISE,KC_ENT),  LT(_RAISE,KC_DOWN),  KC_UP,   M_HENK \
 ),
 
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                |------+------+------+------+------+------|
  * | TILD | EXLM |  AT  | HASH | DLR  | PERC |                | CIRC | AMPR | ASTR | LPRN | RPRN | DEL  |
  * |------+------+------+------+------+------|                |------+------+------+------+------+------|
- * | DEL  |  F1  |  F2  |  F3  |  F4  |  F5  |                |  F6  | UNDS | PLUS | LCBR | RCBR | PIPE |
+ * | DEL  |  F1  |  F2  |  F3  |  F4  |  F5  |                |  F6  | LBRC | RBRC | LCBR | RCBR | PIPE |
  * |------+------+------+------+------+------+------.  ,------+------+------+------+------+------+------|
  * |      |  F7  |  F8  |  F9  | F10  | F11  |      |  |      | F12  |S(KC_N|S(KC_N| LBRC | RBRC |      |
  * `------+------+------+------+------+------+------.  ,------+------+------+------+------+------+------'
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT( \
  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC,   KC_AMPR,   KC_ASTR, KC_LPRN, KC_RPRN, _______, \
  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC,   KC_AMPR,   KC_ASTR, KC_LPRN, KC_RPRN,  KC_DEL,  \
- _______, KC_F1,   KC_F2,   KC_F3,    KC_F4,    KC_F5,                     KC_F6,   KC_UNDS,   KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
+ _______, KC_F1,   KC_F2,   KC_F3,    KC_F4,    KC_F5,                     KC_F6,   KC_LBRC,   KC_RBRC, KC_LCBR, KC_RCBR, KC_PIPE, \
  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11, _______, _______,  KC_F12,S(KC_NUHS),S(KC_NUBS), KC_LBRC, KC_RBRC, _______,\
                  _______, _______,  _______,  _______, _______, _______,  _______, _______, _______, _______\
 ),
