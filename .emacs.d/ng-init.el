0a653325f2d080b5200937c7acc99710f6934887
t_nagoshi <t_nagoshi@timedia.co.jp>
2026-01-08 16:49:06 +0900

    add whitespace

--- .emacs.d/ng-init.el
+++ .emacs.d/ng-init.el
@@ -107,6 +107,38 @@
   (fontaine-set-preset 'regular))
 
 
+(leaf whitespace
+  :doc "minor mode to visualize TAB, (HARD) SPACE, NEWLINE"
+  :tag "builtin"
+  :added "2025-10-23"
+  :init
+  (setq-default whitespace-style
+                '(face spaces empty tabs newline trailing space-mark tab-mark newline-mark))
+  (let* ((ws-lighten 30) ;; Amount in percentage to lighten up black.
+         (ws-color (color-lighten-name "#000000" ws-lighten)))
+    (custom-set-faces
+     `(whitespace-newline                ((t (:foreground ,ws-color))))
+     `(whitespace-missing-newline-at-eof ((t (:foreground ,ws-color))))
+     `(whitespace-space                  ((t (:foreground ,ws-color))))
+     `(whitespace-space-after-tab        ((t (:foreground ,ws-color))))
+     `(whitespace-space-before-tab       ((t (:foreground ,ws-color))))
+     `(whitespace-tab                    ((t (:foreground ,ws-color))))
+     `(whitespace-trailing               ((t (:foreground ,ws-color))))))
+
+  (setq-default whitespace-display-mappings
+                '(
+                  ;; space -> · else .
+                  (space-mark 32 [183] [46])
+                  ;; new line -> ¬ else $
+                  (newline-mark ?\n [172 ?\n] [36 ?\n])
+                  ;; carriage return (Windows) -> ¶ else #
+                  (newline-mark ?\r [182] [35])
+                  ;; tabs -> » else >
+                  (tab-mark ?\t [187 ?\t] [62 ?\t])))
+
+  (setq-default whitespace-action
+                '(cleanup auto-cleanup))
+  )
 
 
 
